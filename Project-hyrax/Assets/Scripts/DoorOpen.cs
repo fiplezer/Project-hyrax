5363369a41156fc15a1220f5fcef4f17b1797c8c
fiplezer <jessebraaksma12@gmail.com>
2026-09-15 18:39:17 +0200

    werkt bijna perfect

--- Project-hyrax/Assets/Scripts/DoorOpen.cs
+++ Project-hyrax/Assets/Scripts/DoorOpen.cs
@@ -1,15 +1,51 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
-public class DoorOpen : MonoBehaviour
+public class DoorOpen : MonoBehaviour, IInteractable
 {
-    public GameObject Holder1;
-    public GameObject Holder2;
-    public GameObject Holder3;
-    private void Update()
-    {
-        if (Holder1.gameObject.transform.eulerAngles.y == 90f)
-        {
+    [Header("Components")]
+    public GameObject Statue1;
+    public GameObject Statue2;
+    public GameObject Statue3;
+    AudioSource audioSource;
 
+    [Header("Audio")]
+    public AudioClip DoorLockSound;
+
+    int statue1Value;
+    int statue2Value;
+    int statue3Value;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void Interact()
+    {
+        Temp_Turning_script statue1 = Statue1.GetComponent<Temp_Turning_script>();
+        Temp_Turning_script statue2 = Statue2.GetComponent<Temp_Turning_script>();
+        Temp_Turning_script statue3 = Statue3.GetComponent<Temp_Turning_script>();
+
+        statue1Value = statue1.value;
+        statue2Value = statue2.value;
+        statue3Value = statue3.value;
+    }
+    void Update()
+    {
+        if (statue1Value == 1f && statue2Value == 2f && statue3Value == 3f && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(DoorLockSound);
+            StartCoroutine(waitOneSeconds());
         }
+
+        return;
+    }
+
+    IEnumerator waitOneSeconds()
+    {
+        yield return new WaitForSeconds(1);
+
+        Destroy(this.gameObject);
     }
 }
--- Project-hyrax/Assets/Scripts/Temp_Turning_script.cs
+++ Project-hyrax/Assets/Scripts/Temp_Turning_script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Temp_Turning_script : MonoBehaviour, IInteractable
 {
     float timer = 0f;
+    public int value = 0;
     void Update()
     {
         timer -= Time.deltaTime;
@@ -13,6 +14,7 @@ public class Temp_Turning_script : MonoBehaviour, IInteractable
         if(timer <= 0)
         {
             transform.Rotate(0, -90, 0);
+            value = (value + 1) % 4;
             timer = 1f;
         }
     }
