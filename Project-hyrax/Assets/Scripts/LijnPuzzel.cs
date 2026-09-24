d9c9e40184d200bf04bd6cd118fb900e086b5972
Dim Klerks <daklerks@st.noorderpoort.nl>
2026-09-24 13:44:12 +0200

    fixed line

--- Project-hyrax/Assets/Scripts/LijnPuzzel.cs
+++ Project-hyrax/Assets/Scripts/LijnPuzzel.cs
@@ -3,41 +3,43 @@ using UnityEngine.AI;
 
 public class LijnPuzzel : MonoBehaviour
 {
-    private LineRenderer line; //to hold the line Renderer
-    public Transform target; //to hold the transform of the target
-    private NavMeshAgent agent; //to hold the agent of this gameObject
+    private LineRenderer line;
+    public Transform target;
+    private NavMeshAgent agent;
     private NavMeshPath paths;
 
     void Start()
     {
-        line = GetComponent<LineRenderer>(); //get the line renderer
-        agent = GetComponent<NavMeshAgent>(); //get the agent
+        //create line stuff
+        line = GetComponent<LineRenderer>();
+        agent = GetComponent<NavMeshAgent>();
         paths = new NavMeshPath();
         getPath();
     }
 
     private void getPath()
     {
-        line.SetPosition(0, transform.position); //set the line's origin
+        line.SetPosition(0, transform.position); 
 
         NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, paths);
 
         DrawPath(paths);
 
-        agent.isStopped = true;  //add this if you don't want to move the agent
+        agent.enabled = false;
+        transform.eulerAngles = new Vector3(90f,0f,0f);
     }
 
     private void DrawPath(NavMeshPath path)
     {
-        if (path.corners.Length < 2) //if the path has 1 or no corners, there is no need
+        if (path.corners.Length < 2)
             return;
 
-        line.positionCount = path.corners.Length; //set the array of positions to the amount of corners
+        line.positionCount = path.corners.Length;
 
         for (var i = 1; i < path.corners.Length; i++)
         {
-            Vector3 linePath = new Vector3(path.corners[i].x, path.corners[i].y + 3, path.corners[i].z);
-            line.SetPosition(i, linePath); //go through each corner and set that to the line renderer's position
+            Vector3 linePath = new Vector3(path.corners[i].x, path.corners[i].y, path.corners[i].z);
+            line.SetPosition(i, linePath); 
         }
     }
 }
